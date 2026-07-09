123c0a29083a37e7c62edf0726a2ca52a63a2ec0
Swadhin <168361244+blueorionn@users.noreply.github.com>
2026-07-09 10:31:16 +0530

    refactor: added new icons, changed icon settings in manifest and layout (close #4)

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,6 +22,29 @@ export const metadata: Metadata = {
   title: TITLE,
   description: DESCRIPTION,
   manifest: '/manifest.json',
+  icons: {
+    icon: [
+      { url: '/icons/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+      { url: '/icons/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+      {
+        url: '/icons/android-chrome-192x192.png',
+        sizes: '192x192',
+        type: 'image/png',
+      },
+      {
+        url: '/icons/android-chrome-512x512.png',
+        sizes: '512x512',
+        type: 'image/png',
+      },
+      { url: '/icons/icon.svg', type: 'image/svg+xml' },
+    ],
+    apple: '/icons/apple-touch-icon.png',
+  },
+  appleWebApp: {
+    title: 'Calcify',
+    capable: true,
+    statusBarStyle: 'black-translucent',
+  },
   openGraph: {
     title: TITLE,
     description: DESCRIPTION,
